692130a0b1740777d73598b22bffacfb1c417d1e
Nelson Otazo <nelson.otazo@gmail.com>
2021-07-14 15:32:03 -0400

    remove unused methods from Plugin.php file

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,9 @@
-<?php namespace Norotaro\Guardian;
+<?php
+
+namespace Norotaro\Guardian;
 
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
-    public function registerComponents()
-    {
-    }
-
-    public function registerSettings()
-    {
-    }
 }
